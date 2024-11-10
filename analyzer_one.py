3d0721f435b1bc1435d5e5974410ffdf884576d5
Vivekchaurasia03 <vivekc03022001@gmail.com>
2024-11-10 18:11:46 -0500

    Editied new developers analyzer

--- analyzer_one.py
+++ analyzer_one.py
@@ -34,6 +34,35 @@ class AnalyzerOne:
         elif self.EXPERIENCE_LEVEL == 'experienced':
             self.analyze_experienced_developers(issues)
 
+    def analyze_new_developers(self, issues: List[Issue]):
+        """
+        Analyzes issues suitable for new developers and plots a graph of labels vs. average time to close.
+        """
+        # Filter quick-to-resolve issues
+        quick_issues = [issue for issue in issues if issue.time_to_close < pd.Series([i.time_to_close for i in issues]).median()]
+        
+        # Calculate average time to close per label
+        label_times = {}
+        for issue in quick_issues:
+            for label in issue.labels:
+                if label not in label_times:
+                    label_times[label] = []
+                label_times[label].append(issue.time_to_close)
+        
+        avg_label_times = {label: sum(times) / len(times) for label, times in label_times.items()}
+        
+        # Plotting
+        labels, avg_times = zip(*sorted(avg_label_times.items(), key=lambda x: x[1]))
+        
+        plt.figure(figsize=(14, 8))
+        plt.bar(labels, avg_times)
+        plt.xlabel('Labels')
+        plt.ylabel('Average Time to Close (hours)')
+        plt.title('Average Time to Close by Label for New Developers')
+        plt.xticks(rotation=45, ha='right')
+        plt.tight_layout()
+        plt.show()
+        
     def analyze_experienced_developers(self, issues: List[Issue]):
         """
         Analyzes issues suitable for experienced developers by creating a bar chart of most active labels
