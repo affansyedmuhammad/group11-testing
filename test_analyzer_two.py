e8e51f064ba60c5c6cca5ec23527b3d7880d456c
Syed Muhammad Affan <affansyedmuhamma@gmail.com>
2024-11-23 13:48:21 -0500

    setup test data

---
+++ test_analyzer_two.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from analyzer_two import AnalyzerTwo
+from model import Issue, Event
+
+class TestAnalyzerTwo(unittest.TestCase):
+
+    def setUp(self):
+        self.analyzer = AnalyzerTwo(mention_threshold=3)
+        event1 = MagicMock()
+        event1.event_type = 'mentioned'
+        event1.author = 'user1'
+        event2 = MagicMock()
+        event2.event_type = 'mentioned'
+        event2.author = 'user2'
+        event3 = MagicMock()
+        event3.event_type = 'mentioned'
+        event3.author = 'user1'
+        issue1 = MagicMock()
+        issue1.events = [event1, event2, event3]
+        event4 = MagicMock()
+        event4.event_type = 'mentioned'
+        event4.author = 'user1'
+        event5 = MagicMock()
+        event5.event_type = 'mentioned'
+        event5.author = 'user3'
+        event6 = MagicMock()
+        event6.event_type = 'opened'
+        event6.author = 'user4'
+
+        issue2 = MagicMock()
+        issue2.events = [event4, event5, event6]
+
+        self.mock_issues = [issue1, issue2]
+
+   
